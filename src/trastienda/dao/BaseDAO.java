5754b742d67d8b0fbd02f3455887e03f09d3335a
kirspas <kirspas@hotmail.com>
2015-05-25 14:50:13 -0500

    bjkdghsf

--- src/trastienda/dao/BaseDAO.java
+++ src/trastienda/dao/BaseDAO.java
@@ -6,7 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-// dao 2
+// dao 245546445644
 public class BaseDAO {
 	
 	protected void cerrarConexion(Connection con) throws RuntimeException {
