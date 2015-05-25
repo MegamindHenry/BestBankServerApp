718839b3abf5ae50ef67475bfbf06a6a7c5e29e3
Erikabn <kc_erika@hotmai.com>
2015-05-25 14:52:47 -0500

    lalala

--- src/trastienda/dao/BaseDAO.java
+++ src/trastienda/dao/BaseDAO.java
@@ -6,7 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-// dao 245546445644
+// dao 245546fsdfs
 public class BaseDAO {
 	
 	protected void cerrarConexion(Connection con) throws RuntimeException {
